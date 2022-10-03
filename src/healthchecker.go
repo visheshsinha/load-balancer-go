f7e9e8eab1fa3a94af7e07211d2eb21508202f9c
Vishesh Sinha <visheshsinha801@gmail.com>
2022-10-04 00:58:03 +0530

    restructuring

---
+++ src/healthchecker.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+
+	"github.com/go-co-op/gocron"
+)
+
+func startHealthCheck() {
+	s := gocron.NewScheduler(time.Local)
+
+	for _, server := range serverList {
+
+		s.Every(2).Second().Do(func() {
+
+		})
+	}
+}
+
+func checkHealth() {
+	
+}
